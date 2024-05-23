345e0ea1dfcf065b041dfd019d8c97aacf705074
kaleab213 <kaleabtibebu400@gmail.com>
2024-05-24 00:24:02 +0300

    chore: Add dependency injection using GetIt

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,27 @@
+import 'package:blog_app/core/secrets/supabse_secrets.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final GetIt serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+      url: SupabaseSecret.supabaseUrl, anonKey: SupabaseSecret.supabaseAnonKey);
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  serviceLocator.registerFactory(() => AuthRemoteDataSource(serviceLocator()));
+  serviceLocator.registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(remoteDataSource: serviceLocator()));
+  serviceLocator
+      .registerFactory(() => UserSignUp(repository: serviceLocator()));
+  serviceLocator
+      .registerLazySingleton(() => AuthBloc(userSignUp: serviceLocator()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,26 @@
-import 'package:blog_app/core/secrets/supabse_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  try {
-    // Initialize Supabase
-    final supabase = await Supabase.initialize(
-        url: SupabaseSecret.supabaseUrl,
-        anonKey: SupabaseSecret.supabaseAnonKey);
+  // Initialize Supabase
+  await initDependencies();
 
-    // final mongoDbClient = await MongoDbClient.connect();
-    // print('mongodb connected succesfully');
-    runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (_) => AuthBloc(
-              userSignUp: UserSignUp(
-                repository: AuthRepositoryImpl(
-                  remoteDataSource: AuthRemoteDataSource(supabase.client),
-                ),
-              ),
-            ),
-          ),
-        ],
-        child: const MyApp(),
-      ),
-    );
-  } catch (e) {
-    print(e.toString());
-  }
+  // final mongoDbClient = await MongoDbClient.connect();
+  // print('mongodb connected succesfully');
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => serviceLocator<AuthBloc>()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
